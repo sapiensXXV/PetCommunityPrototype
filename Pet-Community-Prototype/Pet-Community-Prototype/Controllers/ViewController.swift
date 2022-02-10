45c0250b9241cefc23fa287300862b0936439446
Jaehoon-So <jhspacelover@naver.com>
2022-02-10 19:09:27 +0900

    save project file

---
+++ Pet-Community-Prototype/Pet-Community-Prototype/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Pet-Community-Prototype
+//
+//  Created by Jaehoon So on 2022/02/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
